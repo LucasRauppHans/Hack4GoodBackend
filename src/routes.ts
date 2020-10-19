be35dc5034f321833614ffa9d26f1c115424c09b
Hans <lucas.hans@sap.com>
2020-10-18 22:27:51 -0300

    Backend API

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "./config/upload";
+import OrphanagesController from "./controllers/OrphanagesController";
+
+const routes = Router();
+const uploads = multer(uploadConfig);
+
+routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post(
+   "/orphanages",
+   uploads.array("images"),
+   OrphanagesController.create
+);
+
+export default routes;
